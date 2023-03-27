8a036882a6d7f0616e9480e3525d5804a9d26a5d
Stilwelll <100096883+Stilwelll@users.noreply.github.com>
2023-03-26 20:52:10 -0400

    prototype-v0.0.1
    Added:
    - Menu Scene
    - Game Scene
    ----------------------
    Next Update Focus:
    - Gameplay

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
