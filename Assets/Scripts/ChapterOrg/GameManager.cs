09772a6ad7795d5ae134e9579c5aa0aa9cabecfc
Stilwelll <100096883+Stilwelll@users.noreply.github.com>
2023-03-29 14:51:34 -0400

    prototype- v0.0.5
    Worked on:
    - chapter org scene
    - unit prefab
    - screen resolution scaling
    -----------------------------
    Next Update Focus:
    - art assets
    - random character generation

---
+++ Assets/Scripts/ChapterOrg/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Dictionary<int, GameObject> unitTable = new Dictionary<int, GameObject>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
