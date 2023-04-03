9027c1cec2b704558e429d6a5bcc61d01ea9800e
Stilwelll <100096883+Stilwelll@users.noreply.github.com>
2023-04-03 10:58:05 -0400

    prototype-v0.0.9-1
    Added relevant information to each text box

--- Assets/Scripts/ChapterOrg/UnitGeneration.cs
+++ Assets/Scripts/ChapterOrg/UnitGeneration.cs
@@ -8,7 +8,7 @@ public class UnitGeneration : MonoBehaviour
 {
     public RandomUnitTypes randomUnitTypes;
     public GameManager gameManager;
-    
+
     private Vector3 charOffset;
     public GameObject spaceMarineChar;
     public GameObject content;
@@ -25,7 +25,7 @@ public class UnitGeneration : MonoBehaviour
         // also setting the charcount to that initial char count
         int startingCharCount = Random.Range(5, 10);
         charCount = startingCharCount;
-        
+
         // populating the list with the amount generated above. also increasing the offset with each 
         // -character generated
         for (int i = 0; i < startingCharCount + 1; i++)
@@ -63,27 +63,47 @@ public class UnitGeneration : MonoBehaviour
         GameObject charTab;
         TextMeshProUGUI childsText;
         Transform itemsChild;
+
+        int count;
+        int count2;
+        int randomUnitIndex;
+        int randomUnitIndex2;
         foreach (var item in gameManager.unitTable)
         {
+            // Randomly choosing the names
+
             charTab = gameManager.unitTable[item.Key];
             itemsChild = charTab.transform.Find("Name");
             childsText = itemsChild.GetComponent<TextMeshProUGUI>();
-            childsText.text = "test";
 
+            randomUnitIndex = Random.Range(0, randomUnitTypes.firstName.Length - 1);
+            randomUnitIndex2 = Random.Range(0, randomUnitTypes.firstName.Length - 1);
+
+            childsText.text = randomUnitTypes.firstName[randomUnitIndex] + " " + randomUnitTypes.lastName[randomUnitIndex];
+
+            // Randomly choosing the type
             charTab = gameManager.unitTable[item.Key];
             itemsChild = charTab.transform.Find("Type");
             childsText = itemsChild.GetComponent<TextMeshProUGUI>();
-            childsText.text = "test";
 
+            randomUnitIndex = Random.Range(0, randomUnitTypes.standardTypes.Length - 1);
+            childsText.text = randomUnitTypes.standardTypes[randomUnitIndex];
+
+            // Randomly choosing the company
             charTab = gameManager.unitTable[item.Key];
             itemsChild = charTab.transform.Find("Company");
             childsText = itemsChild.GetComponent<TextMeshProUGUI>();
-            childsText.text = "test";
 
+            randomUnitIndex = Random.Range(0, randomUnitTypes.companies.Length - 1);
+            childsText.text = randomUnitTypes.companies[randomUnitIndex];
+
+            // Randomly choosing the rank
             charTab = gameManager.unitTable[item.Key];
             itemsChild = charTab.transform.Find("Rank");
             childsText = itemsChild.GetComponent<TextMeshProUGUI>();
-            childsText.text = "test";
+
+            randomUnitIndex = Random.Range(0, randomUnitTypes.ranks.Length - 1);
+            childsText.text = randomUnitTypes.ranks[randomUnitIndex];
         }
     }
 }
